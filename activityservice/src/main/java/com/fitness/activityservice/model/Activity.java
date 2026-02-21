6d42746da7a58bdaf69dcaad1529f0792417184b
sefaul-islam <sefaulislam.off@gmail.com>
2026-02-21 21:46:15 +0600

    added new service

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
@@ -0,0 +1,29 @@
+package com.fitness.activityservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Document(collection = "activities")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Activity {
+
+    private String activityId;
+    private String userId;
+    private ActivityType type;
+    private Integer duration;
+    private Integer caloriesBurned;
+    private LocalDateTime startTime;
+    private Map<String,Object> additionalMetrics;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
