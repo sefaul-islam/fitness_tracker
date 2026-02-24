144951752d0519700e30dd84e01e160bbcf7232a
sefaul-islam <sefaulislam.off@gmail.com>
2026-02-24 21:55:22 +0600

    added changes to activity service

--- activityservice/src/main/java/com/fitness/activityservice/ActivityserviceApplication.java
+++ activityservice/src/main/java/com/fitness/activityservice/ActivityserviceApplication.java
@@ -2,10 +2,12 @@ package com.fitness.activityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableMongoAuditing
+@EnableDiscoveryClient
 public class ActivityserviceApplication {
 
 	public static void main(String[] args) {
